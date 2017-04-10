caac782318c9cd43ad7a06cb4d81edcf2aaccc8e
glassjoseph <glassjoseph@gmail.com>
2017-04-10 17:57:34 -0600

    add cart functionality

---
+++ app/models/cart.rb
@@ -0,0 +1,34 @@
+class Cart
+
+  attr_reader :contents
+
+  def initialize(initial_contents)
+     @contents = initial_contents || {}
+     list
+  end
+
+  def add_item(item_id)
+    contents[item_id.to_s] ||= 0
+    contents[item_id.to_s] += 1
+  end
+
+  def list
+    contents.map { |item_id, quantity| OrderItem.new(Item.find(item_id), quantity) }
+  end
+
+  def cart_total
+    list.reduce(0) do |sum, order_item|
+      sum + order_item.subtotal
+    end
+  end
+
+
+  def cart_quantity
+    list.reduce(0) do |sum, order_item|
+      sum + order_item.quantity
+    end
+  end
+
+
+
+end

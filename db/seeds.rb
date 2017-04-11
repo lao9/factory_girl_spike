e8807051ed7baf113e1a64172ddf9ac5e91b393d
Jack Pincus <jwpincus@gmail.com>
2017-04-11 16:01:18 -0600

    Add seeds
    * Seeds are random and will repeat items across categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+titles = ["Pincus's Pickled Pigs Feet", "Oliveri's  Olives", "Mossefin's Pickled Moose", "Glass's Glass Jars", ]
+descriptions = ["Tasty stuff", "The Best", "Pickled goodness", "Addictive, yet lacking nutrition"]
+adjectives = ["sweet", "spicy", "tangy", "dill"]
+prices = Array(1..100)
+
+categories = ["Spicy", "Organic", "Fruit", "Dill", "Meat"]
+categories.each do |category_name|
+  category = Category.create(name: category_name)
+  titles.each do |new_title|
+    category.items.create(title: new_title, description: descriptions.sample, price: prices.sample, image_url: "http://loremflickr.com/320/240/#{adjectives.sample},pickle/all")
+  end
+end

61dad112ce051b900924f3eb42e99a486b06b03e
glassjoseph <glassjoseph@gmail.com>
2017-04-12 10:45:51 -0600

    add edge cases to cart tests

--- spec/features/user_can_add_items_to_cart_spec.rb
+++ spec/features/user_can_add_items_to_cart_spec.rb
@@ -18,23 +18,75 @@ RSpec.feature "User can add items to cart" do
       expect(page).to have_content("Total Price: $#{item.price}")
       expect(page).to have_content("Total Quantity: 1")
     end
+
+    scenario "user can add same item to cart multiple times" do
+      item = create(:item)
+
+      visit items_path
+
+      click_on "Add to Cart"
+      click_on "Add to Cart"
+      click_on "Add to Cart"
+      click_on "Add to Cart"
+
+      expect(page).to have_content("Successfully added #{item.title} to your cart")
+
+      click_on "View Cart"
+
+      expect(current_path).to eq("/cart")
+
+      expect(page).to have_content(item.title)
+      expect(page).to have_content(item.description)
+      expect(page).to have_content(item.price)
+      expect(page).to have_css("img[src*='#{item.image_url}']")
+      expect(page).to have_content("Total Price: $#{item.price * 4}")
+      expect(page).to have_content("Total Quantity: 4")
+
+    end
+
+    scenario "user can add several different items to cart from index" do
+      item1 = create(:item)
+      item2 = create(:item)
+      item3 = create(:item)
+      visit items_path
+
+      within("div#pickle_#{item1.id}.pickle") do
+        click_on "Add to Cart"
+      end
+
+      expect(page).to have_content("Successfully added #{item1.title} to your cart")
+
+      within("div#pickle_#{item2.id}") do
+        click_on "Add to Cart"
+      end
+
+      expect(page).to have_content("Successfully added #{item2.title} to your cart")
+
+      within("div#pickle_#{item3.id}.pickle") do
+        click_on "Add to Cart"
+      end
+
+      expect(page).to have_content("Successfully added #{item3.title} to your cart")
+      click_on "View Cart"
+
+      expect(current_path).to eq("/cart")
+
+      expect(page).to have_content(item1.title)
+      expect(page).to have_content(item1.description)
+      expect(page).to have_content(item1.price)
+      expect(page).to have_css("img[src*='#{item1.image_url}']")
+      expect(page).to have_content(item2.title)
+      expect(page).to have_content(item2.description)
+      expect(page).to have_content(item2.price)
+      expect(page).to have_css("img[src*='#{item2.image_url}']")
+      expect(page).to have_content(item3.title)
+      expect(page).to have_content(item3.description)
+      expect(page).to have_content(item3.price)
+      expect(page).to have_css("img[src*='#{item3.image_url}']")
+      expect(page).to have_content("Total Quantity: 3")
+      expect(page).to have_content("Total Price: $24.0")
+
+
+    end
   end
 end
-
-# edge cases
-# two different items to the cart
-# add one item to the cart and confirm that when we go back and add
-# maybe also add a flash meesage "you added this to your cart"
-
-  # issueFour:
-  #   title: Adding items to the cart
-  #   body: >
-  #     Background: Items, and a user that is not logged in
-  #     As a visitor
-  #     When I visit any page with an item on it
-  #     I should see a link or button for "Add to Cart"
-  #     When I click "Add to cart" for that item
-  #     And I click a link or button to view cart
-  #     And my current path should be "/cart"
-  #     And I should see a small image, title, description and price for the item I just added
-  #     And there should be a "total" price for the cart that should be the sum of all items in the cart

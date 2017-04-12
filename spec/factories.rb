bd1055f6c3b787bab7dadfaf1d22c3751220df76
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-04-11 21:42:21 -0600

    build user factory
    all model tests pass

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,23 @@
 FactoryGirl.define do
 
+  factory :user do
+    sequence :first_name do |n|
+      "Dill #{n}"
+    end
+
+    sequence :last_name do |n|
+      "Pickles #{n}"
+    end
+
+    sequence :email do |n|
+      "picklelover#{n}@pickle.com"
+    end
+
+    sequence :password do |n|
+      "vlassic#{n}"
+    end
+  end
+
   factory :item do
     sequence :title do |n|
       "Pickle #{n}"
@@ -19,7 +37,7 @@ FactoryGirl.define do
 
     factory :item_with_categories do
       categories {create_list(:category, 2)}
-    end 
+    end
   end
 
   factory :category do

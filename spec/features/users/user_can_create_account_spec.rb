1167cdc75aa17b3dc1c012b435a09c9dc8f1f260
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-04-11 20:59:27 -0600

    write feature test for redundant email in sign up

--- spec/features/users/user_can_create_account_spec.rb
+++ spec/features/users/user_can_create_account_spec.rb
@@ -22,5 +22,30 @@ RSpec.feature "User can create a new account" do
       expect(page).to_not have_content("Login")
       expect(page).to_not have_content("Sign Up")
     end
+    scenario "they provide an already existing email" do
+      user = create(:user)
+      redundant_email = user.email
+
+      visit new_user_path
+
+      fill_in "user[first_name]", with: "Dill"
+      fill_in "user[last_name]", with: "Pickles"
+      fill_in "user[email]", with: redundant_email
+      fill_in "user[password]", with: "rugrat2017"
+      fill_in "user[password_confirmation]", with: "rugrat2017"
+
+      click_on "See what the 'big dill' is about!"
+
+      expect(current_path).to eq(new_user_path)
+
+      expect(page).to have_content("#{redundant_email} already taken!")
+
+      expect(page).to have_content("Login")
+      expect(page).to have_content("Sign Up")
+    end
+  end
+  context "when providing not enough information" do
+    # not sure if I need to/how to test this
+    # might be something we can force in the form for
   end
 end

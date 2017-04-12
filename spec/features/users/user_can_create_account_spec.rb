ef9142927a32dc21f46c8597607e40deb5fe9dca
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-04-11 21:18:20 -0600

    write user model tests

--- spec/features/users/user_can_create_account_spec.rb
+++ spec/features/users/user_can_create_account_spec.rb
@@ -49,3 +49,23 @@ RSpec.feature "User can create a new account" do
     # might be something we can force in the form for
   end
 end
+
+#   body: >
+# As a visitor
+# When I visit "/"
+# Then I should see a link for "Login"
+# And when I click "Login"
+# And I should be on the "/login" page
+# I should see a place to insert my credentials to login
+# And I should see a link to "Create Account"
+#
+# As a visitor
+# When I visit "/login
+# And when I click link "Create Account"
+# And I fill in my desired credentials
+# And I submit my information
+# Then my current page should be "/dashboard"
+# And I should see a message in the navbar that says "Logged in as SOME_USER"
+# And I should see my profile information
+# And I should not see a link for "Login"
+# And I should see a link for "Logout"

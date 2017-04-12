882c4918bbec5ea5db6d4af4d3c79c2676efe924
glassjoseph <glassjoseph@gmail.com>
2017-04-12 17:47:27 -0600

    add past orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,28 +15,3 @@ class OrdersController < ApplicationController
     @order_items = @order.order_items
   end
 end
-
-
-  # issueTen:
-  #   title: Viewing a past order
-  #   body: >
-  #     Background: An existing user that has one previous order
-  #     As an authenticated user
-  #     When I visit "/orders"
-  #     Then I should see my past order
-  #     And I should see a link to view that order
-  #     And when I click that link
-  #     Then I should see each item that was order with the quantity and line-item subtotals
-  #     And I should see links to each item's show page
-
-
-
-
-
-
-
-  #     And I should see the current status of the order **(ordered, paid, cancelled, completed)**
-  #     And I should see the total price for the order
-  #     And I should see the date/time that the order was submitted
-  #     If the order was completed or cancelled
-  #     Then I should see a timestamp when the action took place

d3e7654cf55baf8b366b418f023cb143102e63ad
glassjoseph <glassjoseph@gmail.com>
2017-04-13 17:23:56 -0600

    change password validation to password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :first_name, :last_name, :email, :password, presence: true
+  validates :first_name, :last_name, :password_digest, :email, presence: true
   validates :email, uniqueness: true
 
   has_many :orders

aabe06880e4b4083da80ebe0798dca125345e626
glassjoseph <glassjoseph@gmail.com>
2017-04-13 21:47:09 -0600

    redirect admin to dashboard on login
    -closes #75

---
+++ spec/features/admin/admin_sees_dashboard_on_login_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature 'Admin logs in' do
+  it 'admin sees the admin dashboard' do
+    user = User.create(first_name: "George", last_name: "Devenshire", email: "g@d.com", password: "password", role: 1)
+
+    visit login_path
+    fill_in "session[email]", with: user.email
+    fill_in "session[password]", with: user.password
+    click_button "Log In"
+
+    expect(current_path).to eq("/admin/dashboard")
+  end
+end

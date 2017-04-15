a582c89adb013eca16444438385deaf714fc773a
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-04-15 13:56:40 -0600

    fix failing tests
    update price display in cart show
    update price display in order table
    update order tests
    refactor to use user stubs

--- spec/features/orders/user_can_view_their_orders_spec.rb
+++ spec/features/orders/user_can_view_their_orders_spec.rb
@@ -28,6 +28,7 @@ feature "when a user visits their orders page" do
 
       visit orders_path
 
+
       within("table.table") do
         expect(page).to have_link(order.id)
         click_on order.id
@@ -39,12 +40,30 @@ feature "when a user visits their orders page" do
       expect(page).to have_content(item1.price)
 
       page.has_selector?(:xpath, '/html/body/div[2]/div/table/tbody/tr[2]/td[4]')   #
-      expect(page).to have_content("Total Price: $#{order.total_price}")
+      expect(page).to have_content("Total Price: #{order.total_price}")
       expect(page).to have_content("Purchase Date: #{date}")
       expect(page).to have_link(item1.title, href: item_path(item1))
       expect(page).to have_css("img[src*='#{item1.image_url}']")
 
       expect(page).to have_content("Current Status: ordered on #{date}")
+    end
+    scenario "user with several past orders can view them" do
+      user = create(:user)
+      user.orders << create_list(:order_with_many_items, 3)
+
+      allow_any_instance_of(ApplicationController)
+        .to receive(:current_user)
+        .and_return(user)
+
+      visit orders_path
+
+      expect(current_path).to eq(orders_path)
+
+      within("table.table") do
+        expect(page).to have_content(user.orders[0].id)
+        expect(page).to have_content(user.orders[1].id)
+        expect(page).to have_content(user.orders[2].id)
+      end
     end
   end
 end

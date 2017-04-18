024937eab056e914d26ef9a081d914ee19ff7ad9
glassjoseph <glassjoseph@gmail.com>
2017-04-18 08:22:34 -0600

    add item model validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,8 @@ class Item < ApplicationRecord
   has_many :categories, through: :category_items
   has_many :order_items
   has_many :orders, through: :order_items
+
+  validates :price, presence: true, :numericality => {greater_than: 0}
+  validates :description, presence: true
+  validates :title, presence: true, uniqueness: true
 end

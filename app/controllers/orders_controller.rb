b645a233915548527aeb2cb6eeb3ba0b7d37b403
Lauren Oliveri <lauren.oliveri.9@gmail.com>
2017-04-20 07:52:50 -0600

    dry up controllers
    add callback functions for OrderMailers
    add callbacks in order controller for show and update methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-
+  before_action :set_order, only: [:update, :show]
   def index
     unless current_user
       render :file => 'public/404.html', :status => :not_found
@@ -12,26 +12,24 @@ class OrdersController < ApplicationController
     @order = Order.create(user_id: session[:user_id])
     @order.order_items << @cart.list
     session[:cart] = nil
-    OrderMailer.new_order(@order.user, @order).deliver
     flash[:success] = "Your order has been placed!"
     redirect_to order_path(@order)
   end
 
   def show
-    @order= Order.find(params[:id])
     @order_items = @order.order_items
     @display_user = @order.user
   end
 
   def update
-    order = Order.find(params[:id])
-    order.update(status: params[:status])
-    OrderMailer.update(order.user, order).deliver
+    @order.update(status: params[:status])
     redirect_back(fallback_location: root_path)
   end
 
-private
+  private
 
-
+  def set_order
+    @order = Order.find(params[:id])
+  end
 
 end

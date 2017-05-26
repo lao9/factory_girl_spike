0a8d700e9c5cf9d314fb5e2f20afba0a6da996a6
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-05-25 19:22:36 -0600

    cart spec passes

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,28 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  let(:cart) {create(:cart)}
-  let(:item1) {create(:item, id: 1)}
-  let(:item2) {create(:item, id: 2)}
+  #### Replace the Test Setup up with 1 factories
+  # 1. cart
+
+  attr_reader :cart, :item1, :item2
+  before :each do
+    @item1 = Item.create(title: 'Monkey',
+      description: "It's a banana",
+      price:        15.0,
+      image_url:    '/fruits/yellow')
+
+    @item2 = Item.create(title: 'Lemur',
+      description: "It's a banana",
+      price:        15.0,
+      image_url:    '/fruits/yellow')
+
+    @cart = Cart.new({"#{@item1.id}" => 1})
+  end
 
   describe "attributes" do
     it "factory has contents" do
       expect(cart).to respond_to(:contents)
       expect(cart.contents).to be_a Hash
       expect(cart.contents.keys.count).to eq(1)
-      expect(cart.contents["1"]).to eq(1)
+      expect(cart.contents["#{item1.id}"]).to eq(1)
     end
   end
   describe "methods" do
     it "can add new item to cart" do
       cart.add_item(item2.id)
 
-      expect(cart.contents).to eq({"1"=>1, "2"=> 1})
+      expect(cart.contents).to eq({"#{item1.id}"=>1, "#{item2.id}"=> 1})
     end
     it "can new same item to cart" do
       cart.add_item(item1.id)
 
-      expect(cart.contents).to eq({"1"=>2})
+      expect(cart.contents).to eq({"#{item1.id}"=>2})
     end
   end
 end
